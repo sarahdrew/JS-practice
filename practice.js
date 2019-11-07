1f67a7b1857e1e4d7bc727c729ed61463ed23c09
Sarah Drew <sarahdrew93@gmail.com>
2019-11-07 12:51:38 -0500

    completed occurence problem

--- practice.js
+++ practice.js
@@ -1,8 +1,26 @@
-//Write a small program, that given a string as input, counts the number of occurences of each word.
+// 1. Write a small program, that given a string as input, counts the number of occurences letter being looked for.
 
 
 
-//Write a react-native component that animates its content in and out.
+function countOccurence(string, letter) {
+    //occrence starts at 0 then loop through
+    occurence = 0;
+    for (let i = 0; i < string.length; i++) {
+        //if the character where it's looped is the letter that's being looked for, add to the occurence
+        if (string.charAt(i) == letter) {
+            occrence += 1;
+        }
+    }
+    return occurence;
+}
+
+
+
+
+
+
+
+//2. Write a react-native component that animates its content in and out.
 //you need tto implement the <ContnetSwapper value ={} /> component.
 // Requirements:
 // Every time the parent component changes the “value” field on the ContentSwapper component, the new content will we replaced with the old content using the swap animation described above.
